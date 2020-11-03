915c29fa817df0ae9631fb63df172d78fb554d92
Ajay <ajay kumar>
2020-11-03 16:11:12 +0530

    first commit

---
+++ tfresources/main.tf
@@ -0,0 +1,11 @@
+module "resources" {
+    source = "git::https://github.com/shankar5885/module.git?ref=master"
+    rg_name = var.rg_name
+    subnet_name = var.subnet_name
+    vnet_name = var.vnet_name
+    vnet_address_space = var.vnet_address_space
+    subnet_address_space = var.subnet_address_space
+    nic_name = var.nic_na   me
+    vm_name = var.vm_name
+    vm_size = var.vm_size
+}
