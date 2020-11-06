0760185e0bfeafc82890e8554cf839639ebb3225
itsajayraj1989 <73166481+itsajayraj1989@users.noreply.github.com>
2020-11-06 12:21:06 +0530

    Update variables.tf

--- tfresources/variables.tf
+++ tfresources/variables.tf
@@ -1,31 +1,47 @@
 variable "rg_name" {
     type = string
+    default     = "RG01"
+  description = "Name of the resource group"
 }
 
 variable "vnet_name" {
     type = string
+    default     = "VNet01"
+  description = "Name of the Virtual Network"
 }
 
 variable "subnet_name" {
     type = string
+    default     = "SubNet01"
+  description = "Name of the Subnet"
 }
 
 variable "vnet_address_space" {
     type = list(string)
+    default     = "Vnet01"
+  description = "Name of the resource group"
 }
 
 variable "subnet_address_space" {
     type = string
+    default     = "Subnet01"
+  description = "Name of the Subnet Address space"
 }
 
 variable "nic_name" {
     type = string
+    default     = "VM01"
+  description = "Name of the Nic name"
 }
 
 variable "vm_name" {
     type = string
+    default     = "VN01"
+  description = "Name of the Virtual machine"
 }
 
 variable "vm_size" {
     type = string
+    default     = "10GB"
+  description = "Name of the Virtual machine size"
 }
